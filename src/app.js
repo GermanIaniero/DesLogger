37305294ef666ae1360ee988cb91a8c61e8227cb
German Ianiero <german_danielianiero@hotmail.com>
2023-10-13 00:56:52 -0300

    modificado el 13-10-23 00:56 hs

--- src/app.js
+++ src/app.js
@@ -3,9 +3,28 @@ import userRouter from './routes/user.router.js'
 import mockRouter from './routes/mock.router.js'
 import errorHandler from './middlewares/error.js'
 
+import { addLogger, logger } from './utils/logger.js'
+
 const app = express()
+
+app.use(addLogger)
+
 app.use('/api/users', userRouter)
 app.use('/api/mockingproducts', mockRouter)
 app.use(errorHandler)
+
+app.get('/loggerTest', (req, res) => {
+
+    req.logger.fatal(`FATAL !!! `)
+    req.logger.error(`Se cayo el server 👎 `)
+    req.logger.warning(`Don't worry. Is just a warning`)
+    req.logger.info(`Se llamo a esta url`)
+    req.logger.debug(`1 +1 === 2`)
+
+    res.send('Logger Testing')
+})
+
+app.get('/', (req, res) => res.send('Logger Testing'))
+app.post('/', (req, res) => res.send('Logger Testing POST'))
 
 app.listen(8080, () => console.log('Listening...'))
